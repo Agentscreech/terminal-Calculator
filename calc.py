63276efb8a110d68443260f2fc38ea7f23896fe5
Agentscreech <exoticimage@hotmail.com>
2016-07-06 01:00:36 -0700

    added tests for valid inputs and printed out function as input
    should be fully functional.  Still doesn't do proper order of
    operations.  It's just doing FIFO operations.

--- calc.py
+++ calc.py
@@ -1,46 +1,72 @@
-#! python3
-# op = input("what do you want to do? ")
-qty = input("how many numbers are we dealing with? ")
+# ! python 3
+# This program will take inputs one at a time from the user and do the operation
+# instructed in the order it was given then print out that function with the result.
+
+
+def getNums(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Sorry, that wasn't a number, try again")
+            continue
+        else:
+            return userInput
+            break
+qty = getNums('How many numbers are we dealing with ')
 numLst = []
 opLst = []
 for i in range(int(qty)):
-    numLst.append(input("next number "))
-    opLst.append(input('what are we doing with it? (+,-,*,/,^) '))
-#TODO still need to find a way to check if the input was valid
-#before trying to iterate the array
-print(numLst)
-#TODO find a way to stop asking for the operator on the last number
-#this just deletes the last one for now
-opLst = opLst[0:-1]
-print(opLst)
-
-
-total = 0
-n = 0
-for i in numLst:
-    # print(i)
-    # print(opLst[n])
-    if opLst[n] == "+":
-            total += int(i)
+    if i == 0:
+        numLst.append(getNums("First Number "))
+    elif i == int(qty) - 1:
+        numLst.append(getNums("Last Number "))
+    else:
+        numLst.append(getNums("Next number "))
+    if i != (int(qty)) - 1:
+        while True:
+            opLst.append(input('what are we doing with it? (+,-,*,/,^) '))
+            if opLst[i] not in ("+", "-", "*", "/", "^"):
+                print("Not an appropriate choice.")
+                del opLst[i]
+            else:
+                break
+total = int(numLst[0])
+n = 1
+print(total, end=''),
+for i in opLst:
+    if i == "+":
+            total += int(numLst[n])
+            print(i, end='')
+            print(numLst[n], end='')
             n += 1
-    elif opLst[n] == "-":
-            total -= int(i)
+    elif i == "-":
+            total -= int(numLst[n])
+            print(i, end='')
+            print(numLst[n], end='')
             n += 1
-    elif opLst[n] == "*":
+    elif i == "*":
             if total == 0:
                 total = 1
-            total *= int(i)
+            total *= int(numLst[n])
+            print(i, end='')
+            print(numLst[n], end='')
             n += 1
-    elif opLst[n] == "/":
+    elif i == "/":
             if total == 0:
                 total = 1
-            total /= int(i)
+            total /= float(int(numLst[n]))
+            print(i, end='')
+            print(numLst[n], end='')
             n += 1
-    elif opLst[n] == "^":
+    elif i == "^":
             if total == 0:
                 total = 1
-            total ^= int(i)
+            total ^= int(numLst[n])
+            print(i, end='')
+            print(numLst[n], end='')
             n += 1
     else:
         print ("I don't know how to do that")
+print("=", end=''),
 print(total)
